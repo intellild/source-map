a5865301b1ccbd6ae1baa8204f750d1cc6ac3860
intellild <intellild@outlook.com>
2026-05-18 09:24:37 +0000

    Update source map JSON dependencies

--- parcel_sourcemap/src/lib.rs
+++ parcel_sourcemap/src/lib.rs
@@ -744,7 +744,7 @@ impl SourceMap {
     #[cfg(feature = "json")]
     pub fn to_data_url(&mut self, source_root: Option<&str>) -> Result<String, SourceMapError> {
         let buf = self.to_json(source_root)?;
-        let b64 = base64_simd::Base64::STANDARD.encode_to_boxed_str(buf.as_bytes());
+        let b64 = base64_simd::STANDARD.encode_to_string(buf.as_bytes());
         Ok(format!(
             "data:application/json;charset=utf-8;base64,{}",
             b64
